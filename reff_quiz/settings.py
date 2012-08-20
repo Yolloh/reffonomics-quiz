fc13182d8ea308ac36835308d6725f88a1da68dc
Roey Chasman <rchasman@email.arizona.edu>
2012-08-19 21:19:52 -0700

    Setup base.html template with bootstrap and default styling, also fixed url and view issues.

--- reff_quiz/settings.py
+++ reff_quiz/settings.py
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'reff_quiz.urls'
+ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'reff_quiz.wsgi.application'
@@ -109,7 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    PATH + 'templates'
+    PATH + 'templates/',
 )
 
 INSTALLED_APPS = (
